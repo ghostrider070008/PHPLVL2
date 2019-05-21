b1e85bb2105718d10aaff20415f39ca7d9de878b
ghostrider07008 <ghostrider07008@gmail.com>
2019-05-21 16:57:53 +0300

    Первое задание готово

--- goods.class.php
+++ goods.class.php
@@ -6,34 +6,37 @@ class Goods
     public $id;
     public $name;
     public $price;
-    public $opis;
+    public $desc;
     public $count;
 
-    function __construct($id, $name, $price, $opis, $count)
+    function __construct($id, $name, $price, $desc, $count)
     {
         $this->id = $id;
         $this->name = $name;
         $this->price = $price;
-        $this->opis = $opis;
+        $this->desc = $desc;
         $this->count = $count;
     }
     function getName(){
-        echo "<div><h2>Наименование товара: $this->name</h2></div>";
+        return $this->name;
     }
     function getPrice(){
-        echo "<div><h2>Наименование товара: $this->name</h2></div>";
+        return $this->price;
     }
-    function getName(){
-        echo "<div><h2>Наименование товара: $this->name</h2></div>";
+    function getDesc(){
+        return $this->desc;
     }
-    function getName(){
-        echo "<div><h2>Наименование товара: $this->name</h2></div>";
+    function getCount(){
+        return $this->count;
     }
 }
 $g = new Goods(1, 'Ноутбук', 25000, 'Технические требования для ноутбука', 5);
 $g2 = new Goods (2, 'Мышь', 250, 'Оптическая', 10);
 $g3 = new Goods (3, 'Клавиатура', 400, 'Проводная', 15);
-$array = [$g, $g2];
-$array[] = $g3;
-print_r($array);
-$g->view();
+$mas = [$g, $g2];
+$mas[] = $g3;
+print_r($mas);
+foreach ($mas as $obj){
+    echo "<h2>Наименование:".$obj->getName()."</h2><br>Цена:".$obj->getPrice()."<br>Описание:".$obj->getDesc()."<br>Доступное количество:".$obj->getCount();
+}
+
