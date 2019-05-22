b18bd0eec98c02ca9767a04e308bc320b7c5130c
ghostrider07008 <ghostrider07008@gmail.com>
2019-05-22 16:46:57 +0300

    продвижение с 4 заданием

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-require_once('goods.class.php');
-
+//require_once('goods.class.php');
+include ('printer.php');
--- printer.php
+++ printer.php
@@ -1,7 +1,22 @@
 <?php
 
 require_once("goods.class.php");
-class printer extends Goods
-{
+class Printer extends Goods
+{   private $speed_print;
+    private $format;
+    private $spec;
+
+    public function __construct($id, $name, $price, $desc, $count,$speed_print, $format)
+    {
+        parent::__construct($id, $name, $price, $desc, $count);
+        $this->spec = [
+            "Скорость печати" => $speed_print
+    ];
+        $this->speed_print = $speed_print;
+        $this->format = $format;
+    }
+
 
 }
+$k = new Printer(1,"Принтер", 20540, "printer", 2, 20, "A4");
+print_r($k);
